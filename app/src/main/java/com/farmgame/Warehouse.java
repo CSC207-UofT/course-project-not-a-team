ec040402f02d4a66f46a091db80170b6d03ee848
Jason-YW-L <Liuyw6273008@gmail.com>
2021-10-10 18:26:48 -0400

    Added warehouse class

---
+++ app/src/main/java/com/farmgame/Warehouse.java
@@ -0,0 +1,17 @@
+package com.farmgame;
+
+public class Warehouse {
+    private int capacity;
+
+    public Warehouse(){
+        this.capacity = 5;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capcity) {
+        this.capacity = capcity;
+    }
+}
