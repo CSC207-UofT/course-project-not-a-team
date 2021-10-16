784e86afab328ebc2e9e1fddcd816092c95ccb51
Hanyu Zhu <harriet.zhu@mail.utoronto.ca>
2021-10-15 22:20:53 -0400

    added comments

--- app/src/main/java/com/farmgame/Store.java
+++ app/src/main/java/com/farmgame/Store.java
@@ -11,25 +11,36 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Store {
-    List<Plants> totalProducts;             //we need to define
+    List<Plants> totalProducts;             //we need to define later
     int StoreLevel;
     List<Plants> currentProducts;
     /**
-     * User can use store to buy plants, sell plants. The price varies each day.
+     * User can use store to buy plants, sell plants. The price varies each day. Sold plants are
+     * automatically stored into warehouse.
      * totalProducts is a List of plants sells in store in TOTAL
-     * currentProducts List of plants that sells in store(current level)
+     * currentProducts ArrayList of plants that sells in store(current level)
      * @param StoreLevel an integer that shows the level of the store
      */
     public Store( int StoreLevel){
+        //constructor, take StoreLevel as parameter, initiate ArrayList currentProducts given the StoreLevel
         this.StoreLevel = StoreLevel;
-        currentProducts = new List<Plants>();
+        currentProducts = new ArrayList<>();
         currentProducts = totalProducts.subList(0, StoreLevel);
 
     }
     public List<Plants> getCurrentProducts(){
+        //return current products
+
         return currentProducts;
     }
+    public void setCurrentProducts(int level){
+        // set the current products according to given level
+        for(int i = 0; i < level; i++) {
+            currentProducts.set(i, totalProducts.get(i));
+        }
+    }
     public int getStoreLevel(){
+        //return store level
         return StoreLevel;
     }
     /*
