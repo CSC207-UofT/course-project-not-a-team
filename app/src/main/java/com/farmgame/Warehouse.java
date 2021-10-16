f703bf8279439469d7a64cce6e29bec1e5ba4f1c
Jason-YW-L <Liuyw6273008@gmail.com>
2021-10-16 16:31:25 -0400

    warehouse class update

--- app/src/main/java/com/farmgame/Warehouse.java
+++ app/src/main/java/com/farmgame/Warehouse.java
@@ -1,10 +1,24 @@
 package com.farmgame;
 
+import com.farmgame.Item.Item;
+
+import java.util.ArrayList;
+
 public class Warehouse {
     private int capacity;
+    private ArrayList<Item> inventory;
 
     public Warehouse(){
         this.capacity = 5;
+        this.inventory = new ArrayList<Item>();
+    }
+
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Item> inventory) {
+        this.inventory = inventory;
     }
 
     public int getCapacity() {
