c3ddef41b65e0e69be032ab8d1e88f7a52cf25a8
Hanyu Zhu <harriet.zhu@mail.utoronto.ca>
2021-10-16 15:23:39 -0400

    All warnings clear

--- app/src/main/java/com/farmgame/Store.java
+++ app/src/main/java/com/farmgame/Store.java
@@ -1,17 +1,14 @@
 package com.farmgame;
 
-import android.annotation.SuppressLint;
+
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
+
 import java.util.List;
-import java.text.SimpleDateFormat;
-import java.util.Random;
-import java.util.Scanner;
+
 
 public class Store {
-    List<Plants> totalProducts;             //we need to define later
+    //List<Plants> totalProducts;             //we need to define later
     int StoreLevel;
     List<Plants> currentProducts;
     /**
@@ -25,14 +22,14 @@ public class Store {
         //constructor, take StoreLevel as parameter, initiate ArrayList currentProducts given the StoreLevel
         this.StoreLevel = StoreLevel;
         currentProducts = new ArrayList<>();
-        currentProducts = totalProducts.subList(0, StoreLevel);
+        // currentProducts = totalProducts.subList(0, StoreLevel);
 
     }
-    public List<Plants> getCurrentProducts(){
+    //public List<Plants> getCurrentProducts(){
         //return current products
 
-        return currentProducts;
-    }
+        //return currentProducts;
+    //}
     public int getStoreLevel(){
         //return store level
         return StoreLevel;
@@ -40,48 +37,7 @@ public class Store {
     public void setStoreLevel(int level){
         //set store level
         this.StoreLevel = level;
-        currentProducts = totalProducts.subList(0, level);
-    }
-    /*
-    public List<Plants> buyingProducts(){
-        Date date = Calendar.getInstance().getTime();
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat currentDate = new SimpleDateFormat("ddMMyyyy");
-        Random rand = new Random(Integer.parseInt(currentDate.format(date)));
-
-        for (Plants item: currentProducts){
-            System.out.println(item.getName, item.getPrice * rand);
-        }
-        List<Plants> result = new List<Plants>();
-
-
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter the plant you want to buy and seperate by space: ");
-        String[] input = reader.nextLine().split(" ");
-        reader.close();
-        for (String element : input) {
-            for (Plants plant : currentProducts) {
-                if (plant.getName == element) {
-                    result.add(plant);
-                    user.purchaseAmount(plant.getPrice*rand);
-                }
-            }
-        }
-
-        System.out.println("The products you bought are:");
-        for (Plants plant : result){
-            System.out.println(plant.getName);
-        }
-        return result;
-
+        //currentProducts = totalProducts.subList(0, level);
     }
 
-    public int sellingProducts(List<Plants> plants4sell){
-        int price = 0;
-        for(Plants item: plants4sell){
-            price += item.getPrice;
-        }
-        return price;
-    }
-
-     */
 }
