ee833faf132b840ba4aae6aca9fcb921cc0c8699
11768 <zac.yang@mail.utoronto.ca>
2021-10-24 21:38:18 -0400

    Name changed.

--- app/src/main/java/com/farmgame/Plants.java
+++ app/src/main/java/com/farmgame/Plants.java
@@ -1,11 +1,11 @@
 package com.farmgame;
 
 public class Plants {
-    private final String PlantName;
-    private final int PlantingTime;
-    private final int BuyingPrice;
-    private final int SellingPrice;
-    private final int ExperiencePoint;
+    private final String FINAL_PLANTTIME;
+    private final int FINAL_PLANTINGTIME;
+    private final int FINAL_BUYINGPRICE;
+    private final int FINAL_SELLINGPRICE;
+    private final int FINAL_EXPERIMENTPOINT;
 
     // PlantName -- Name of plant
     // PlantingTime -- The time it takes for a plant to mature
@@ -14,36 +14,36 @@ public class Plants {
     // ExperiencePoint -- Experience points gained while harvesting plants
 
     public Plants(String name, int time, int buying, int selling, int exp) {
-        this.PlantName = name;
-        this.PlantingTime = time;
-        this.BuyingPrice = buying;
-        this.SellingPrice = selling;
-        this.ExperiencePoint = exp;
+        this.FINAL_PLANTTIME = name;
+        this.FINAL_PLANTINGTIME = time;
+        this.FINAL_BUYINGPRICE = buying;
+        this.FINAL_SELLINGPRICE = selling;
+        this.FINAL_EXPERIMENTPOINT = exp;
     }
     // Initialize the plant.
 
     public String getPlantName(){
-        return this.PlantName;
+        return this.FINAL_PLANTTIME;
     }
     // Get the PlantName of object
 
     public int getPlantingTime(){
-        return this.PlantingTime;
+        return this.FINAL_PLANTINGTIME;
     }
     // Get the PlantingTime of object
 
     public int getSellingPrice(){
-        return this.SellingPrice;
+        return this.FINAL_SELLINGPRICE;
     }
     // Get the SellingPrice of object
 
     public int getBuyingPrice(){
-        return this.BuyingPrice;
+        return this.FINAL_BUYINGPRICE;
     }
     // Get the BuyingPrice of object
 
     public int getExperiencePoint(){
-        return this.ExperiencePoint;
+        return this.FINAL_EXPERIMENTPOINT;
     } // Get the ExperiencePoint of object
 
 
