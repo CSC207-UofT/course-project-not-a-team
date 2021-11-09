2ae4492b777523213458787fde120495bc4be0bd
Jingquan Cao (Connor) <jingquan.cao@mail.utoronto.ca>
2021-11-09 18:15:10 -0500

    update item classes, 11.06.06

--- app/src/main/java/com/farmgame/entity/Item/WateringCan.java
+++ app/src/main/java/com/farmgame/entity/Item/WateringCan.java
@@ -3,6 +3,7 @@ package com.farmgame.entity.Item;
 public class WateringCan implements Item{
     private int price = 5;
 
+    @Override
     public int getPrice() { return price; }
 
     @Override
