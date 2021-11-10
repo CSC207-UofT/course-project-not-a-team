f375634f7ff98fd7a95c455d2ffd966d356ee05c
ZacYiYang <zac.yang@mail.utorontoca>
2021-11-10 15:03:53 -0500

    Change plants and add seeds.

--- app/src/main/java/com/farmgame/Plants.java
+++ app/src/main/java/com/farmgame/Plants.java
@@ -1,42 +1,42 @@
 package com.farmgame;
 
 public class Plants {
-//
-//    private final String PLANTS_NAME;
-//    private final int SELLING_PRICE;
-//
-//    /**
-//     * Constructor for plants.
-//     * @param selling -- The price of the plant when you sell it
-//     */
-//
-//    public Plants(String name, int selling) {
-//        this.PLANTS_NAME = name;
-//        this.SELLING_PRICE = selling;
-//    }
-//    /**
-//            * Getter for PLANTS_NAME attributes.
-//            *
-//            * @return String
-//     */
-//
-//    public String getPlantName() {
-//
-//        return this.PLANTS_NAME;
-//    }
-//
-//    /**
-//     * Getter for SELLING_PRICE attributes.
-//     *
-//     * @return int
-//     */
-//
-//    public int getSellingPrice(){
-//
-//        return this.SELLING_PRICE;
-//    }
-//
-//
-//
+
+    private final String PLANTS_NAME;
+    private final int SELLING_PRICE;
+
+    /**
+     * Constructor for plants.
+     * @param selling -- The price of the plant when you sell it
+     */
+
+    public Plants(String name, int selling) {
+        this.PLANTS_NAME = name;
+        this.SELLING_PRICE = selling;
+    }
+    /**
+            * Getter for PLANTS_NAME attributes.
+            *
+            * @return String
+     */
+
+    public String getPlantName() {
+
+        return this.PLANTS_NAME;
+    }
+
+    /**
+     * Getter for SELLING_PRICE attributes.
+     *
+     * @return int
+     */
+
+    public int getSellingPrice(){
+
+        return this.SELLING_PRICE;
+    }
+
+
+
 
 }
--- app/src/test/java/com/farmgame/PlantsTest.java
+++ app/src/test/java/com/farmgame/PlantsTest.java
@@ -5,17 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PlantsTest {
-    //Plants Potato = new Plants("Potato", 60);
+    Plants Potato = new Plants("Potato", 60);
 
-    //@Test(timeout = 50)
-    //public void test_name(){
-    //    assertEquals(Potato.getPlantName(), "Potato");
-    //}
+    @Test(timeout = 50)
+    public void test_name(){
+        assertEquals(Potato.getPlantName(), "Potato");
+    }
 
-    //@Test(timeout = 50)
-    //public void test_selling(){
-    //    assertEquals(Potato.getSellingPrice(), 60);
-    //}
+    @Test(timeout = 50)
+    public void test_selling(){
+        assertEquals(Potato.getSellingPrice(), 60);
+    }
 
 }
 
