5f307d72e74cfd2d9b0d216ff28dce7ff7611066
Severus <xuhui.chen@mail.utoronto.ca>
2021-11-11 22:46:07 -0500

    add method headers get current store product list

--- app/src/main/java/com/farmgame/gateway/StoreDBApi.java
+++ app/src/main/java/com/farmgame/gateway/StoreDBApi.java
@@ -1,4 +1,23 @@
 package com.farmgame.gateway;
 
+import com.farmgame.entity.Item.Item;
+import com.farmgame.entity.Plants;
+import com.farmgame.entity.Seeds;
+
+import java.util.ArrayList;
+
 public class StoreDBApi extends DataBaseAPI {
+
+    public static ArrayList<Plants> getPlantList(){
+
+    }
+
+    public static ArrayList<Seeds> getSeedList(){
+
+    }
+
+    public static ArrayList<Item> getItemList(){
+
+    }
+
 }
