7636fbe6aba928f758f6c03b6d924c791db4325a
SophieLiao0109 <saifei.liao@mail.utoronto.ca>
2021-11-12 01:30:47 -0500

    Test getCurrentProducts_plants

--- app/src/test/java/com/farmgame/entity/StoreUnitTest.java
+++ app/src/test/java/com/farmgame/entity/StoreUnitTest.java
@@ -2,6 +2,8 @@ package com.farmgame.entity;
 
 import static org.junit.Assert.*;
 
+import com.farmgame.entity.Item.Fertilizer;
+import com.farmgame.entity.Item.WateringCan;
 import com.farmgame.entity.Store;
 import com.farmgame.entity.Item.Item;
 import com.farmgame.entity.Plants;
@@ -15,11 +17,27 @@ import java.util.ArrayList;
 public class StoreUnitTest {
     Plants potato = new Plants("Potato", 60, 12);
     Plants berry = new Plants("Berry", 60, 11);
-    Seeds potatoSeed = new Seeds("PotatoSeed", 10, 10,10,10);
-    Seeds bananaSeed = new Seeds("BananaSeed", 10, 10,10,9);
     ArrayList<Plants> plantsArrayList = new ArrayList<>();
     ArrayList<Seeds> seedsArrayList = new ArrayList<>();
     ArrayList<Item> itemArrayList = new ArrayList<>();
+    Store store = new Store(plantsArrayList, seedsArrayList, itemArrayList);
+    Seeds potatoSeed = new Seeds("PotatoSeed", 10, 10,10,10);
+    Seeds bananaSeed = new Seeds("BananaSeed", 10, 10,10,9);
+    Fertilizer fertilizer = new Fertilizer();
+    WateringCan wateringCan = new WateringCan();
+
+    @Test(timeout = 50)
+        public void test_getCurrentProducts_plants(){
+            this.plantsArrayList.add(potato);
+            this.plantsArrayList.add(berry);
+            assertEquals(this.plantsArrayList, store.getCurrentProducts_plants());
+
+
+        }
+    }
+
+
+
 //    Store test_store = new Store(Player());
 //    @Test
 //    public void test_GetStoreLevel() {
