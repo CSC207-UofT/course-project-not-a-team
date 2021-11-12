93aec3455cc8114b5cc888c159b267bb8a50a99f
SophieLiao0109 <saifei.liao@mail.utoronto.ca>
2021-11-12 01:21:32 -0500

    Add new import

--- app/src/test/java/com/farmgame/entity/StoreUnitTest.java
+++ app/src/test/java/com/farmgame/entity/StoreUnitTest.java
@@ -3,14 +3,23 @@ package com.farmgame.entity;
 import static org.junit.Assert.*;
 
 import com.farmgame.entity.Store;
+import com.farmgame.entity.Item.Item;
+import com.farmgame.entity.Plants;
+import com.farmgame.entity.Seeds;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 
 public class StoreUnitTest {
     Plants potato = new Plants("Potato", 60, 12);
     Plants berry = new Plants("Berry", 60, 11);
     Seeds potatoSeed = new Seeds("PotatoSeed", 10, 10,10,10);
     Seeds bananaSeed = new Seeds("BananaSeed", 10, 10,10,9);
+    ArrayList<Plants> plantsArrayList = new ArrayList<>();
+    ArrayList<Seeds> seedsArrayList = new ArrayList<>();
+    ArrayList<Item> itemArrayList = new ArrayList<>();
 //    Store test_store = new Store(Player());
 //    @Test
 //    public void test_GetStoreLevel() {
