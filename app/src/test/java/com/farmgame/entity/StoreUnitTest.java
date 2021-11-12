bde8d2e54ee6cc623529890958fe7877a03311cb
SophieLiao0109 <saifei.liao@mail.utoronto.ca>
2021-11-12 01:19:04 -0500

    Add StoreUnitTest

--- app/src/test/java/com/farmgame/entity/StoreUnitTest.java
+++ app/src/test/java/com/farmgame/entity/StoreUnitTest.java
@@ -7,6 +7,10 @@ import com.farmgame.entity.Store;
 import org.junit.Test;
 
 public class StoreUnitTest {
+    Plants potato = new Plants("Potato", 60, 12);
+    Plants berry = new Plants("Berry", 60, 11);
+    Seeds potatoSeed = new Seeds("PotatoSeed", 10, 10,10,10);
+    Seeds bananaSeed = new Seeds("BananaSeed", 10, 10,10,9);
 //    Store test_store = new Store(Player());
 //    @Test
 //    public void test_GetStoreLevel() {
