0d888d49a3759c6b439c6858e620d04bd558297e
Jason-YW-L <Liuyw6273008@gmail.com>
2021-11-14 00:02:47 -0500

    HomeFragment landButtonClick logic implementation

--- app/src/main/java/com/farmgame/Activity/main/HomeFragment.java
+++ app/src/main/java/com/farmgame/Activity/main/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.farmgame.Activity.main;
 
+import static com.farmgame.constants.Constants.LOCK_STATUS_BOUGHT;
+import static com.farmgame.constants.Constants.LOCK_STATUS_NOT_BOUGHT;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +20,7 @@ import com.farmgame.R;
 import com.farmgame.databinding.FragmentHomeBinding;
 import com.farmgame.entity.LandEntity;
 import com.farmgame.gateway.LandDBApi;
+import com.farmgame.viewModel.MainViewModel;
 
 import java.util.ArrayList;
 
@@ -39,6 +43,7 @@ public class HomeFragment extends Fragment {
     private void populateLandButtons() {
         LandDBApi landDBApi = new LandDBApi();
         ArrayList<LandEntity> landList = landDBApi.getLandList();
+        MainViewModel mainViewModel = new MainViewModel();
         for(int row = 0; row < ROW_NUM; row ++){
             TableLayout table = binding.tableForLandButtons;
             TableRow tableRow = new TableRow(requireActivity());
@@ -61,25 +66,27 @@ public class HomeFragment extends Fragment {
 
                 int finalCol = col;
                 int finalRow = row;
+                LandEntity land = mainViewModel.getLand(row * col);
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        landButtonClick(finalRow, finalCol);
+                        landButtonClick(finalRow, finalCol, land);
                     }
                 });
             }
         }
     }
-    private void landButtonClick(int col, int row){
+    private void landButtonClick(int col, int row, LandEntity land){
         Toast.makeText(requireActivity(), col + "," + row + "Has been Clicked", Toast.LENGTH_SHORT).show();
-
-        // if this land is unlocked and purchased
+        if (land.getLockStatus() == LOCK_STATUS_BOUGHT){
             //show attributes(plant, waterTime, etc...
             //show action buttons: harvest, water, fertilize...
-        // Else if this land is unlock and not purchased
+        }else if(land.getLockStatus() == LOCK_STATUS_NOT_BOUGHT){
             // show buy button
-        // else
-            // show text message
+        }else{
+            Toast.makeText(requireActivity(), "You have not unlocked this land, please level up and try again.", Toast.LENGTH_SHORT).show();
+        }
+
     }
     @Override
     public void onDestroyView() {
