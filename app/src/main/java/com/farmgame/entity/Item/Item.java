75cbda86849abe31863e37a5cc67a036cece3c2c
Jingquan Cao (Connor) <jingquan.cao@mail.utoronto.ca>
2021-11-13 19:25:34 -0500

    V11.13.02: rewrite Item.java by making it a class instead of a interface

--- app/src/main/java/com/farmgame/entity/Item/Item.java
+++ app/src/main/java/com/farmgame/entity/Item/Item.java
@@ -1,14 +1,32 @@
 package com.farmgame.entity.Item;
 
 import com.farmgame.entity.LandEntity;
+import com.farmgame.usecase.StoreAble;
 
-public interface Item {
+public abstract class Item implements StoreAble {
+    private final int price;
+    private final String item_name;
+    private final int id;
 
-    int getPrice();
+    public Item(int price, String item_name, int id) {
+        this.price = price;
+        this.item_name = item_name;
+        this.id = id;
+    }
 
-    int getId();
+    public int getPrice() {
+        return price;
+    }
 
-    String getName();
+    public int getId() {
+        return id;
+    }
 
-    void use(LandEntity land);
+    public String getName() {
+        return item_name;
+    }
+
+    public abstract String getType();
+
+    public abstract void use(LandEntity land);
 }
