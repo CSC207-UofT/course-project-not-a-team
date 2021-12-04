d908c2d2737d429b67446418b94e7a9d66f030c3
Severus <xuhui.chen@mail.utoronto.ca>
2021-12-04 17:43:36 -0500

    complete front end of auto harvest

--- app/src/main/java/com/farmgame/frontEnd/main/LandFragment.java
+++ app/src/main/java/com/farmgame/frontEnd/main/LandFragment.java
@@ -1,5 +1,6 @@
 package com.farmgame.frontEnd.main;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.os.Bundle;
 
@@ -35,6 +36,7 @@ import java.util.ArrayList;
 /**
  * the fragment of the land page (first tab page), in MainActivity
  */
+@SuppressLint("InflateParams")
 public class LandFragment extends Fragment {
 
 
@@ -49,9 +51,8 @@ public class LandFragment extends Fragment {
 
 
         binding = FragmentLandBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        return root;
+        return binding.getRoot();
     }
 
 
@@ -64,6 +65,8 @@ public class LandFragment extends Fragment {
         viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
 
         binding.playerInfo.setText(playerInfo());
+
+        binding.harvestAll.setOnClickListener(v -> viewModel.getLHS().auto_harvest());
 
         viewModel.playerData.observe(requireActivity(), player -> {
             if (this.isVisible()){
@@ -168,6 +171,7 @@ public class LandFragment extends Fragment {
      * set the window of planting actions(watering, harvest, fertilize) of a land
      * @param land the land that the actions to be executed on
      */
+    @SuppressLint("SetTextI18n")
     private void setActionWindow(LandEntity land){
         View contentView = LayoutInflater.from(requireActivity()).inflate(R.layout.land_action_popup, null);
         PopupWindow popupWindow = new PopupWindow(contentView, ViewGroup.LayoutParams.MATCH_PARENT,
@@ -213,10 +217,8 @@ public class LandFragment extends Fragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
         builder.setMessage("Buy the land? Cost: " + land.getPrice())
                 .setPositiveButton(R.string.confirm, (dialog, which)
-                                -> {
-                    Toast.makeText(requireActivity(), viewModel.getLCS().buyLand(land.getIndex()),
-                            Toast.LENGTH_SHORT).show();
-                        }
+                                -> Toast.makeText(requireActivity(), viewModel.getLCS().buyLand(
+                                        land.getIndex()), Toast.LENGTH_SHORT).show()
                 )
                 .setNegativeButton(R.string.cancel, null)
                 .create().show();
